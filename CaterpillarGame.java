0e2c68f356914f67b01690a4690b252501549c1d
Monita Mitra <31115172+monitamitra@users.noreply.github.com>
2023-06-09 08:59:23 -0500

    Add files via upload

---
+++ CaterpillarGame.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class CaterpillarGame {
+
+	public CaterpillarGame() throws IOException {
+		// launches the Caterpillar game from the beginning
+		new CaterpillarFrame().launchGame();
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		// calls on constructor (Java executes the main method
+		new CaterpillarGame();
+	}
+
+}
